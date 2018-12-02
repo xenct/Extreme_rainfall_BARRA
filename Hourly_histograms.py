2008d3f15b004e19f9d805099c8ec74c287a3c17
xenct <45543810+xenct@users.noreply.github.com>
2018-12-03 10:22:51 +1100

    Create Hourly_histograms.py
    Outline the process for plotting hourly histograms

---
+++ Hourly_histograms.py
@@ -0,0 +1,15 @@
+# Hourly histograms
+# initialise python packages
+
+# Get AWS hourly data
+
+# Get BARRA hourly data
+
+# For each station make a plot of the two overlapping distributions
+
+# filter BARRA for the same days/hours available from the AWS
+
+# have the same bin width and scale for all plots
+
+# --
+# compare for seasons?
